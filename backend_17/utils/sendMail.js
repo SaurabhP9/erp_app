843ebfdbd6a5809b89d9759fd4874349f21b1806
Pranav <pranav@techtogether.in>
2025-09-15 23:42:04 +0530

    integrate SendGrid for eamil service

--- backend_17/utils/sendMail.js
+++ backend_17/utils/sendMail.js
@@ -1,27 +1,16 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  socketTimeout: 60000,
-});
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (to, subject, text, html, cc = []) => {
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
+  const msg = {
     to,
     cc,
+    from: process.env.EMAIL_USER, // Must be a verified sender in SendGrid
     subject,
     text,
     html,
   };
-
-  return transporter.sendMail(mailOptions);
+  await sgMail.send(msg);
 };
 
 module.exports = sendEmail;
