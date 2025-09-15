0eb42a8c30b0fb67983f9576da08262f8be2f3c2
Pranav <pranav@techtogether.in>
2025-09-15 23:15:01 +0530

    fixed email service

--- backend_17/utils/sendMail.js
+++ backend_17/utils/sendMail.js
@@ -1,16 +1,19 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  socketTimeout: 60000,
 });
 
 const sendEmail = async (to, subject, text, html, cc = []) => {
   const mailOptions = {
-    from: process.env.MAIL_USER,
+    from: process.env.EMAIL_USER,
     to,
     cc,
     subject,
