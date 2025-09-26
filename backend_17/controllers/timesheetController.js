d5e85857f6294a2b86fb743cff3c9c1aa54f6b90
SaurabhP9 <saurabh125pathare@gmail.com>
2025-09-26 18:09:00 +0530

    remove one mail id

--- backend_17/controllers/timesheetController.js
+++ backend_17/controllers/timesheetController.js
@@ -945,11 +945,8 @@ exports.triggerEmailForEmployeeTimeSheet = async () => {
     const plainText = `Consolidated timesheet summary for ${today}`;
     // 🔹 Hardcoded email IDs
     const toEmail = "development@clickerpservices.com";
-    const ccEmails = [
-      "yogesh.kale@clickerpservices.com",
-      "prasad.chilwar@clickerpservices.com",
-    ];
-    
+    const ccEmails = ["prasad.chilwar@clickerpservices.com"];
+
     return await sendEmail(toEmail, subject, plainText, html, ccEmails);
   } catch (err) {
     console.error("Error sending consolidated timesheet email:", err);
